65298d36f737671c73e122eb123c84fe7b32c7c1
dev-abhishekkr <dev.abhishekkr@gmail.com>
2024-10-21 02:15:18 +0530

    added prisma and api routes

---
+++ app/app/api/streams/upvote/route.ts
@@ -0,0 +1,45 @@
+import { prismaClient } from "@/app/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+
+const UpvoteSchema = z.object({
+    streamId: z.string(),
+})
+
+export async function POST(req: NextRequest) {
+    const session = await getServerSession();
+    //TODO: we can get rid of db call here
+
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email: session?.user?.email ?? ""
+        }
+    })
+    if (!user) {
+        return NextResponse.json({
+            message: "user unauthenticated"
+        }, {
+            status: 402
+        })
+    }
+
+    try {
+        const data = UpvoteSchema.parse(await req.json())
+        await prismaClient.upvote.create({
+            data: {
+                userId: user.id,
+                streamId: data.streamId
+            }
+        })
+
+    } catch (error) {
+        return NextResponse.json({
+            message: "Error while upvoting"
+        }, {
+            status: 403
+        })
+    }
+
+}
